26df2710613f1ff882fbbbf12ca5a3695ab8a75f
deadmau5 <ashwat16023@iiitd.ac.in>
2021-10-05 00:08:39 +0530

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,8 @@
+import random
+
+r1 = random.randint(0, 10)
+print("Random number between 0 and 10 is % s" % (r1))
+  
+
+r2 = random.randint(-10, -1)
+print("Random number between -10 and -1 is % d" % (r2))
